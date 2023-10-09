560d14d1b733ce720b89a71fc2c588c829c61d77
Przemek <przemyslaw.nosek1@gmail.com>
2023-10-09 19:01:37 +0200

    api gateway and eureka discovery implemented

---
+++ api-gateway/src/main/java/pl/healthylifestyle/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,11 @@
+package pl.healthylifestyle.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args); //todo git init
+    }
+}
