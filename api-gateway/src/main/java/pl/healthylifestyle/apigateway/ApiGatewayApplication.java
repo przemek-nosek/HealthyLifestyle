c899bb36e422edca03487b47f830dc9413fdea25
Przemek <przemyslaw.nosek1@gmail.com>
2023-10-09 19:02:28 +0200

    removed todo

--- api-gateway/src/main/java/pl/healthylifestyle/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/pl/healthylifestyle/apigateway/ApiGatewayApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args); //todo git init
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
