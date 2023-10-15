fd2eca94bf0ee1bb68becfb34dc554be696b61cf
Przemek <przemyslaw.nosek1@gmail.com>
2023-10-15 21:31:30 +0200

    created docker compose for keycloak, postgres for keycloak and pgadmin

--- additional-user-data-service/src/main/java/pl/healthylifestyle/userservice/AdditionalUserDataServiceApplication.java
+++ additional-user-data-service/src/main/java/pl/healthylifestyle/userservice/AdditionalUserDataServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //todo: config server
-//todo: keycloak with postgres
 //todo: create docker for application
 //todo: observability https://www.youtube.com/watch?v=PT2yZTBnUwQ
 @SpringBootApplication
