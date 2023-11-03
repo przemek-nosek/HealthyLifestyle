865e93db4e35c2717ebab2f30b1e726608671521
Przemek <przemyslaw.nosek1@gmail.com>
2023-11-03 23:56:53 +0100

    HL0004 - get and update user data

--- additional-user-data-service/src/main/java/pl/healthylifestyle/userservice/controller/AdditionalUserDataEndpoint.java
+++ additional-user-data-service/src/main/java/pl/healthylifestyle/userservice/controller/AdditionalUserDataEndpoint.java
@@ -7,8 +7,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import pl.healthylifestyle.userservice.dto.AdditionalUserDataDto;
-import pl.healthylifestyle.userservice.dto.AdditionalUserDataRequest;
+import pl.healthylifestyle.userservice.dto.CreateAdditionalUserDataRequest;
 import pl.healthylifestyle.userservice.dto.AdditionalUserDataResponse;
+import pl.healthylifestyle.userservice.dto.UpdateAdditionalUserDataRequest;
 import pl.healthylifestyle.userservice.mapper.AdditionalUserDataMapper;
 import pl.healthylifestyle.userservice.service.AdditionalUserDataService;
 
@@ -20,14 +21,33 @@ class AdditionalUserDataEndpoint {
     private final  AdditionalUserDataService additionalUserDataService;
     private final  AdditionalUserDataMapper additionalUserDataMapper;
 
-    @PostMapping("/{uuid}")
-    @PreAuthorize("#uuid == #jwt.name")
-    ResponseEntity<AdditionalUserDataResponse> createAdditionalUserData(
+    @GetMapping("/{uuid}")
+    @PreAuthorize("hasAnyRole('admin', 'ceo') or #uuid == #jwt.name")
+    ResponseEntity<AdditionalUserDataResponse> getAdditionalUserData(
             @PathVariable String uuid,
-            @Valid @RequestBody AdditionalUserDataRequest request,
-            JwtAuthenticationToken jwt) {
+            JwtAuthenticationToken jwt
+    ) {
+        return ResponseEntity.ok(additionalUserDataService.getAdditionalUserData(uuid));
+    }
 
-        AdditionalUserDataDto additionalUserDataDto = additionalUserDataMapper.toAdditionalUserDataDto(request, uuid);
+    @PostMapping
+    @PreAuthorize("#request.uuid == #jwt.name")
+    ResponseEntity<AdditionalUserDataResponse> createAdditionalUserData(
+            @Valid @RequestBody CreateAdditionalUserDataRequest request,
+            JwtAuthenticationToken jwt
+    ) {
+        AdditionalUserDataDto additionalUserDataDto = additionalUserDataMapper.toAdditionalUserDataDto(request);
         return ResponseEntity.ok(additionalUserDataService.createAdditionalUserData(additionalUserDataDto));
+    }
+
+    @PutMapping("/{uuid}")
+    @PreAuthorize("#uuid == #jwt.name")
+    ResponseEntity<AdditionalUserDataResponse> updateAdditionalUserData(
+            @PathVariable String uuid,
+            @Valid @RequestBody UpdateAdditionalUserDataRequest request,
+            JwtAuthenticationToken jwt
+    ) {
+        AdditionalUserDataDto additionalUserDataDto = additionalUserDataMapper.toAdditionalUserDataDto(request, uuid);
+        return ResponseEntity.ok(additionalUserDataService.updateAdditionalUserData(additionalUserDataDto));
     }
 }
--- additional-user-data-service/src/main/java/pl/healthylifestyle/userservice/exception/message/AdditionalUserDataExceptionMessage.java
+++ additional-user-data-service/src/main/java/pl/healthylifestyle/userservice/exception/message/AdditionalUserDataExceptionMessage.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum AdditionalUserDataExceptionMessage {
 
-    USER_DATA_ALREADY_EXISTS_ERROR("User additional data %s already exists");
+    USER_DATA_ALREADY_EXISTS_ERROR("User additional data %s already exists"),
+    USER_DATA_NOT_FOUND_ERROR("User additional data %s not found");
 
     private final String message;
 }
