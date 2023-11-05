04fa6d2ef3a5e1daa7a12533f0b5758ad9923a20
Przemek Nosek <86568433+przemek-nosek@users.noreply.github.com>
2023-11-05 21:23:06 +0100

    HL-0004 - additional user data controller unit tests (#10)
    * HL-0004 - unit tests for service
    
    * HL-0004 - additional user data controller unit tests
    
    ---------
    
    Co-authored-by: Przemek <przemyslaw.nosek1@gmail.com>

--- additional-user-data-service/src/main/java/pl/healthylifestyle/userservice/dto/UpdateAdditionalUserDataRequest.java
+++ additional-user-data-service/src/main/java/pl/healthylifestyle/userservice/dto/UpdateAdditionalUserDataRequest.java
@@ -1,5 +1,6 @@
 package pl.healthylifestyle.userservice.dto;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -13,6 +14,7 @@ import java.util.Set;
 public record UpdateAdditionalUserDataRequest(
         String phoneNumber,
         @Min(value = 13, message = "Age can't be lower than {value}.")
+        @Max(value = 100, message = "Age can't be higher than {value}.")
         int age,
         @NotNull(message = "Gender can't be null.")
         Gender gender,
