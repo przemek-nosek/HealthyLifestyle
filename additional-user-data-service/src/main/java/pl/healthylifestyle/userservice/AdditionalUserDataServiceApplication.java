a78cf04a7f3fb9d7339b727164e94b97be0f500d
Przemek <przemyslaw.nosek1@gmail.com>
2023-11-30 22:50:45 +0100

    cors fix

--- additional-user-data-service/src/main/java/pl/healthylifestyle/userservice/AdditionalUserDataServiceApplication.java
+++ additional-user-data-service/src/main/java/pl/healthylifestyle/userservice/AdditionalUserDataServiceApplication.java
@@ -5,14 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //todo: config server/bootstrap properties
-//todo: create docker for application
 //todo: observability https://www.youtube.com/watch?v=PT2yZTBnUwQ
 //todo: observability https://www.youtube.com/watch?v=mPPhcU7oWDU
 //todo: zipkin
 //todo: measurement properties/move measurement to separate microservices
 //todo: measurement add validation for draft & ability to modification
 //todo: postman collection
-//todo: auditing
+//todo: rename food to produkt spozywczy
+//todo: food - size - unit(slice, ML, kg...), start making sql init with foods
 @SpringBootApplication
 public class AdditionalUserDataServiceApplication {
     public static void main(String[] args) {
