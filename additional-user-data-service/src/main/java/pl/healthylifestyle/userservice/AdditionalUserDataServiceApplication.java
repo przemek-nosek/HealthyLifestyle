d5a9cbc0c291191ebd9a2548a29b433a50325030
Przemek <przemyslaw.nosek1@gmail.com>
2023-12-11 21:36:20 +0100

    ci-pipeline

--- additional-user-data-service/src/main/java/pl/healthylifestyle/userservice/AdditionalUserDataServiceApplication.java
+++ additional-user-data-service/src/main/java/pl/healthylifestyle/userservice/AdditionalUserDataServiceApplication.java
@@ -6,13 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //todo: config server/bootstrap properties
 //todo: observability https://www.youtube.com/watch?v=PT2yZTBnUwQ
-//todo: observability https://www.youtube.com/watch?v=mPPhcU7oWDU
 //todo: zipkin
-//todo: measurement properties/move measurement to separate microservices
 //todo: measurement add validation for draft & ability to modification
-//todo: postman collection
-//todo: rename food to produkt spozywczy
-//todo: food - size - unit(slice, ML, kg...), start making sql init with foods
 @SpringBootApplication
 public class AdditionalUserDataServiceApplication {
     public static void main(String[] args) {
